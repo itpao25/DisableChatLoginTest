d0082021b9c24ab1514786792a4fc187c9cb1939
itpao25 <info@paolo-trombini.it>
2015-06-08 04:33:01 +0200

    Added config.yml for set messages Added command /NewMcStoryLogin Reload (con il permesso NewMCStoryLogin.reload)

--- src/it/itpao25/NewMCStoryLogin/LoginChat.java
+++ src/it/itpao25/NewMCStoryLogin/LoginChat.java
@@ -4,6 +4,8 @@ package it.itpao25.NewMCStoryLogin;
 import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,23 +21,72 @@ public class LoginChat extends JavaPlugin
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static LoginChat plugin;
 	
-	public void onEnable(){
+	public void onEnable() {
 		
 		getLogger().info("NewMcStoryLogin abilitato");
 		PluginManager manager = getServer().getPluginManager();
 	    manager.registerEvents(this, this);
-		
+	    loadConfiguration();
+	    
 	}
 	
+    public void loadConfiguration() {
+    	
+    	this.getConfig().options().copyDefaults(true); 
+        this.saveDefaultConfig();
+        
+    }     
+    
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+    	if(label.equalsIgnoreCase("NewMcStoryLogin")) 
+    	{
+    		if (sender instanceof Player) {
+        
+    			Player playerSend = (Player)sender;
+        
+    			if (args.length == 0) 
+    			{
+
+    				playerSend.sendMessage(ChatColor.RED + this.getConfig().getString("messaggi.nessun-argomento"));
+        		
+    			}
+    			else if (args.length == 1) 
+    			{
+    				if (args[0].equalsIgnoreCase("reload")) {
+    					if (playerSend.hasPermission("NewMCStoryLogin.reload") | (playerSend.isOp()))  
+    					{
+    						
+    						
+    						loadConfiguration();
+    		    			this.reloadConfig();
+    		    			this.saveConfig();
+    		    			
+    		    			playerSend.sendMessage(ChatColor.GREEN + this.getConfig().getString("messaggi.reload-completo"));
+    		    			
+    					} 
+    					else {
+    		    				
+    						playerSend.sendMessage(ChatColor.RED + this.getConfig().getString("messaggi.nessun-permesso"));
+    					}
+    				}
+    			}
+    		}
+    	}
+		return false;
+    	
+    }
+      
 	@EventHandler
 	public void onPlayerChat(AsyncPlayerChatEvent event)
     {
 		Player player = event.getPlayer();
 		
-		if ((!player.hasPermission("NewMCStoryLogin.Bypass")) || (!player.isOp())) 
+		if (!player.hasPermission("NewMCStoryLogin.Bypass") | (!player.isOp()))  
 		{
-			event.setCancelled(true);	
-			player.sendMessage(ChatColor.RED + "Non hai permesso di usare la chat in quest'area, buttati nel portale");	
+				event.setCancelled(true);	
+				player.sendMessage(ChatColor.RED + "Non hai permesso di usare la chat in quest'area, buttati nel portale");	
+
 		}
 		
      } 
